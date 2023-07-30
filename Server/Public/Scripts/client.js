005bcadd1a77a5f65f5afa50b5e22b59f67aae84
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-07-30 15:37:36 -0500

    confirmed GET and render function working

--- Server/Public/Scripts/client.js
+++ Server/Public/Scripts/client.js
@@ -2,13 +2,14 @@ $(document).ready(OnReady)
 
 function OnReady (){
 console.log('Yay, in OnReady!!!')
+getTaskList();
 // Handlers
 $('#add2do-btn').on('click', addToDo)
 
 // Auto run GET function to display table results
+
 }
 let taskList;
-
 
 function addToDo (){
     console.log('testing addToDo 1234')
@@ -20,23 +21,25 @@ function getTaskList (){
     $.ajax({ 
         type: 'GET',
         url: '/to-do-list'
-    }).then((response) => {
-        console.log(response);
-       taskList = response;
+    }).then( function (response) {
+        taskList = response;
+        render(taskList)
     }).catch((error) => {
         console.log("error with GET,", error);
     })
 }
 
 
-// Render to update DOM
+// Render function
 function render(){
-    for (toDo of taskList){
-        console.log('in render', taskList)
+    $("#viewTaskData").empty();
+
+    // console.log('in render', taskList)
+    for (let i = 0; i < taskList.length; i++){
         let newRow = $(`
         <tr>
-        <td>${toDo.task}</td>
-        <td>${toDo.complete}</td>
+        <td>${taskList[i].task}</td>
+        <td>${taskList[i].complete}</td>
         <td>
             <button class="Complete-btn">
             Complete
@@ -49,5 +52,5 @@ function render(){
         </td>`)
 
         $('#viewTaskData').append(newRow);
-    }
+    };
 }

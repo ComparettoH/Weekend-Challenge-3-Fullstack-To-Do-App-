d5cc8fdf5120bea1db6404d12d412d9295250b60
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-07-30 12:30:01 -0500

    troubleshoot GET request and render function

--- Server/Public/Scripts/client.js
+++ Server/Public/Scripts/client.js
@@ -3,6 +3,51 @@ $(document).ready(OnReady)
 function OnReady (){
 console.log('Yay, in OnReady!!!')
 // Handlers
+$('#add2do-btn').on('click', addToDo)
+
+// Auto run GET function to display table results
+}
+let taskList;
 
 
+function addToDo (){
+    console.log('testing addToDo 1234')
+}
+
+// GET function
+function getTaskList (){
+    console.log('in getTaskList')
+    $.ajax({ 
+        type: 'GET',
+        url: '/to-do-list'
+    }).then((response) => {
+        console.log(response);
+       taskList = response;
+    }).catch((error) => {
+        console.log("error with GET,", error);
+    })
+}
+
+
+// Render to update DOM
+function render(){
+    for (toDo of taskList){
+        console.log('in render', taskList)
+        let newRow = $(`
+        <tr>
+        <td>${toDo.task}</td>
+        <td>${toDo.complete}</td>
+        <td>
+            <button class="Complete-btn">
+            Complete
+            </button>
+        </td>
+        <td>
+            <button class="btn-Delete">
+            Delete
+            </button>
+        </td>`)
+
+        $('#viewTaskData').append(newRow);
+    }
 }
--- Server/Server.js
+++ Server/Server.js
@@ -3,22 +3,36 @@ const express = require('express');
 // Require body parser
 const bodyParser = require('body-parser');
 // Require pg
-// const pg = require('pg'); <-- add once database is running
+const pg = require('pg'); 
+// <-- add once database is running
 // 
 // 
 const app = express();
 const PORT = 5000;
-// const pool = new pg.Pool({
-//     database: '',
-//     host: 'localhost',
-//     port: 5432,
-// }); <---- add once database is running
+const pool = new pg.Pool({
+    database: 'weekend-to-do-app',
+    host: 'localhost',
+    port: 5432,
+}); 
+// <---- add once database is running
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 //------------------------------------------
 // Section for Ajax logic/paths
-// 
+
+// First create GET request to confirm connectivity
+app.get('/to-do-list', (req, res) => {
+    let queryText = `SELECT * FROM "toDoTable";`;
+
+    pool.query(queryText).then((result) => {
+        console.log(result.rows);
+        res.send(result.rows)
+    }).catch((error) => {
+        console.log(`Error on GET query ${queryText}`, error);
+        res.sendStatus(500);
+    })
+})
 // 
 // 
 // 
